26b85a097358a1271ab2a7b48f2f40449cb7b2e2
CalixtoTech <103325008+ClxtMf@users.noreply.github.com>
2023-01-07 19:37:36 -0300

    Update Atvd_function.js

--- Atividades/Atvd_function.js
+++ Atividades/Atvd_function.js
@@ -63,189 +63,216 @@ questao5()
 
 // Relembrando aula passada
 
-/**
- * function <nome> (<argumento>){
- *     <código>
- * }
- * 
- * -> chamando função
- * <nome> (<argumento>);
- */
-
-
-// Alterando endereço
-// function autenticar() {
-//     let usuario = prompt('Digite o seu usuário: ');
-//     let senha = prompt('Digite sua senha: ');
-
-//     if(usuario == 'admin' && senha == '1234') {
-//        alert('dados confirmados!!!')
-//     } else {
-//        alert('Dados incorretos!')
-//     }
-// }
-
-//Alterando foto
-// autenticar()
-//Solicitar apagar conta
-// autenticar()
-//Alterando endereço
-// autenticar()
-
-
-// Aula 05 - 710
-
-// 1. Crie uma função que leia a distancia percorrida e o consumo do veiculo. Em seguida calcule quantos lts de gasolina o veiculo gastou na viagem.
-
-function lts_gasolina() {
-    let distancia = Number(prompt('Digite a distancia percorrida pelo carro: '));
-    let consumo = Number(prompt('Digite o consumo por km do carro: '));
-
-    let consumoTotal = distancia/consumo;
-    alert(`Seu veiculo gastou ${consumoTotal}`);
+/*
+function <nome>(<argumentos>) {
+    <código>
 }
-lts_gasolina()
 
-// 2. Crie uma função que calcula a velocidade média de um veiculo. Para isso leia a distancia e o tempo da viagem. 
+-> chamando uma função
+<nome>(<argumentos>);
+*/
+
+function autenticar() {
+    let usuario = prompt('Digite o seu usuário: ');
+    let senha = prompt('Digite a sua senha: ');
+    if (usuario == 'admin' && senha == '123') {
+        alert('Dados confirmados!');
+    }
+    else {
+        alert('Dados incorretos!');
+    }
+}
+// alterando o endereço
+autenticar();
+
+// mudar a foto
+autenticar();
+
+// apagar a conta
+autenticar();
+
+---------------------------------------------------------------------------------------------
+
+1. Crie uma função que leia a distancia percorrida e o consumo do veiculo. Em seguida calcule quantos lts de gasolina o veiculo gastou na viagem.
+
+function calculoConsumo(){
+    let distancia = Number(prompt('Digite a distância percorrida:'));
+    let consumo = Number(prompt('Informe o consumo do veiculo:'));
+
+    let consumoTotal = distancia / consumo;
+    alert(`Seu veiculo gastou ${consumoTotal}lts`);
+}
+
+calculoConsumo();
+*****************************************************************************************************
+2. Crie uma função que calcula a velocidade média de um veiculo. Para isso leia a distancia e o tempo da viagem. 
 
 function velocidadeMedia() {
-    let distancia = Number(prompt('Informe a distancia percorrida: '))
+    let distancia = Number(prompt('Informe a distância percorrida:'))
     let tempo = Number(prompt('Informe o tempo da viagem: '))
 
-    let velocidaMedia = distancia/tempo;
-    alert(`Velocidade média: ${velocidaMedia}km/h`)
-}
-velocidadeMedia()
+    let velocidadeMedia = distancia / tempo;
 
-// 3. Crie uma função que leia base maior, base menor, altura. E em seguida calcule a área de um trapézio. 
-// area = (base maior + base menor) * altura / 2
+    alert(`Velocidade Média: ${velocidadeMedia}km/h`);
+}
+
+velocidadeMedia();
+***********************************************************************
+3. Crie uma função que leia base maior, base menor, altura. E em seguida calcule a área de um trapézio. 
+area = (base maior + base menor) * altura / 2
 
 function calcTrapezio() {
-    let baseMaior = Number(prompt('Informe a base maior: '))
-    let baseMenor = Number(prompt('Infomer a base menor: '))
-    let altura = Number(prompt('Informe a altura do trápezio: '))
+    let baseMaior = Number(prompt('Informe a base maior:'));
+    let baseMenor = Number(prompt('Digite a base menor:'));
+    let altura = Number(prompt('Digite a altura do trapézio:'));
 
     let areaTrapezio = ((baseMaior + baseMenor) * altura) / 2;
     alert(`Área do trapézio = ${areaTrapezio}`);
 }
-calcTrapezio()
 
-// 4. Crie uma função que imprima uma sequencia de números de 0 a 50. Utilize o laço for
+calcTrapezio();
+
+*********************************************************************
+
+4. Crie uma função que imprima uma sequencia de números de 0 a 50. Utilize o laço for
 
 function contagem() {
     for(let i = 0; i <= 50; i++) {
         console.log(i);
     }
 }
+
 contagem();
+************************************************************************
 
-
-
-
-//   parâmetros ou argumentos
-
-// 01 -  parâmetros
-function calcConsumo(distancia, consumo) {
-    let consumoTotal = distancia/consumo;
+// 1. Crie uma função que leia a distancia percorrida e o consumo do veiculo. Em seguida calcule quantos lts de gasolina o veiculo gastou na viagem.
+// Criando a função com parâmetros
+function calcConsumo(distancia, consumo) { 
+    // distancia = 240 consumo = 12
+    let consumoTotal = distancia / consumo;
     alert(`Consumo total = ${consumoTotal}lts`);
 }
+
 calcConsumo(240, 12);
 
-// 02 - parâmetros
+***********************************************************************************
+
+// 2. Crie uma função que calcula a velocidade média de um veiculo. Para isso leia a distancia e o tempo da viagem. 
+
 function velocidadeMedia(distancia, tempo) {
     let velocidade = distancia / tempo;
     alert(`Velocidade Média: ${velocidade}km/h`);
 }
+
 velocidadeMedia(200, 2);
 
-// 03 - parãmetros
-function calcTrapezio(baseMaior, baseMenor, altura) {
+***********************************************************************************
+
+1. Crie uma função que recebe três parâmetros: base maior, base menor, altura. Em seguida calcule e exiba a área do trapézio. 
+
+function calcArea(baseMaior, baseMenor, altura) {
     let areaTrapezio = ((baseMaior + baseMenor) * altura) / 2;
     alert(`Área do trapézio = ${areaTrapezio}`);
 }
-calcTrapezio()
 
+let base_maior = 8;
+let base_menor = 5;
+let height = 4;
+calcArea(base_maior, base_menor, height);
 
-//   parâmetros ou argumentos
+************************************************
+1. Criue uma função que recebe 3 parametros (2 inteiros e um real) em seguida calcule e mostre.
+a. O produto (multiplicação) do dobro do primeiro com a metade do segundo
+b. A soma do triplo do primeiro com o terceiro
+c. O terceiro ao cubo
 
-// 01 -  parâmetros
-function calcConsumo(distancia, consumo) {
-    let consumoTotal = distancia/consumo;
-    alert(`Consumo total = ${consumoTotal}lts`);
-}
-calcConsumo(240, 12);
+****************************************************************
 
-// 02 - parâmetros
-function velocidadeMedia(distancia, tempo) {
-    let velocidade = distancia / tempo;
-    alert(`Velocidade Média: ${velocidade}km/h`);
-}
-velocidadeMedia(200, 2);
-
-// 03 - parãmetros
-function calcTrapezio(baseMaior, baseMenor, altura) {
-    let areaTrapezio = ((baseMaior + baseMenor) * altura) / 2;
-    alert(`Área do trapézio = ${areaTrapezio}`);
-}
-calcTrapezio()
-
-// Atividades
-
-// 01. Escreva uma função que recebe um nome como parâmetro e imprima uma mensagem “Olá” + nome
-
-function nome(nome){
-    alert(`Olá ${nome}, seja bem-vindo!!!`);
-}
-nome('Márcio');
-
-// 02. Escreva uma função que recebe um lado como parâmetro, calcule a área desse quadrado e exiba no console. 
-
-function area(lado){
-    let area = lado ** 2;
-    alert(area)
-}
-area(10)
-
-// 03. Escreva uma função que recebe dois parâmetros, base e altura. Em seguida calcula a área desse triângulo e a exiba na tela.
-
-function areaTri(base, altura){
-    let area = base * altura / 2;
-    alert(area)
-}
-areaTri(10, 5)
-
-// 04. Escreva uma função que recebe um número e imprima todos os inteiros de 0 até este número recebido.
-
-function contagem(numero){
-    for(let i = 0; i <= numero; i++);
-    alert(i);
-}
-contagem(10);
-
-// 05. Escreva uma função que recebe um número como parâmetros e imprima todos os pares de 0 até essenúmero.
-
-function contagemPar(numero){
-    for(let i = 0; i <= numero; i++);
-    if(i % 2 == 0 ){
-        alert(i)
-    } 
-}
-contagemPar(10);
-
-
-
-// 6. Criue uma função que recebe 3 parametros (2 inteiros e um real) em seguida calcule e mostre.
-// a. O produto (multiplicação) do dobro do primeiro com a metade do segundo
-// b. A soma do triplo do primeiro com o terceiro
-// c. O terceiro ao cubo
-
-function calculo(primeiro, segundo, terceiro) {
-    let resultado1 = (primeiro * 2) * (segundo / 2);
-    let resultado2 = (primeiro * 3) + terceiro;
-    let resultado3 = terceiro ** 3;
-    alert(resultado1);
-    alert(resultado2);
-    alert(resultado3);
+// Criando uma função com retorno
+function calculoConsumo(distancia, consumo) {
+    let consumoTotal = distancia / consumo
+    return consumoTotal; // 20
 }
 
-calculo(10, 5, 3.5);
+
+let consumoViagem = calculoConsumo(240, 12); // 20
+alert(consumoViagem);
+*******************************************************************
+
+// Crie uma função que recebe duas notas por parâmetro e retorne aprovado se a média for maior que 6 e recuperação senão.
+
+// function calcularMedia(nota1, nota2) {
+//     let media = (nota1 + nota2) / 2;
+//     if (media >= 6) {
+//         return 'aprovado';
+//     }
+//     else {
+//         return 'recuperação';
+//     }
+// }
+// let resultado = calcularMedia(6, 6);
+// alert(resultado);
+
+// Crie uma função que recebe três números como parâmetro e retorne o maior deles
+// function verificaMaior(num1, num2, num3) {
+//     if (num1 > num2 && num1 > num3) {
+//         return num1;
+//     } else if (num2 > num1 && num2 > num3) {
+//         return num2;
+//     } else {
+//         return num3;
+//     }
+// }
+
+// let maiorNumero = verificaMaior(10, 2, 7);
+// alert(maiorNumero);
+
+// Crie uma função que recebe a idade e um booleano indicando se o usuário tem cnh. Se o usuário possuir cnh e for maior de idade retorne “voce pode dirigir” senão, retorne "você não pode dirigir.
+
+function podeDirigir(idade, cnh) {
+    if (idade >= 18 && cnh == true) {
+        return "Pode dirigir";
+    }
+    else {
+        return "Você não pode dirigir";
+    }
+}
+let habilitado = podeDirigir(18, true);
+alert(habilitado);
+*************************************************************************************
+
+
+// const areaQuadrado = (lado) => {
+//     return lado ** 2;
+// }
+
+// // chamando uma arrow function
+
+// alert(areaQuadrado(5));
+
+// const areaTriangulo = (base, altura) => {
+//     let area = base * altura / 2;
+//     return area;
+// }
+
+// alert(areaTriangulo(5, 8));
+
+// funções de escopo simples não precisam de return nem chaves
+const areaQuadrado = (lado) => lado ** 2;
+alert(areaQuadrado(5));
+----------------------------------------------------------------------------------------------
+1. Crie uma função que leia a distancia percorrida e o consumo do veiculo. Em seguida calcule quantos lts de gasolina o veiculo gastou na viagem. Utilize arrow function
+
+const calculoConsumo = (distancia, consumo) => {
+    return distancia / consumo;
+}
+alert(calculoConsumo(240, 12));
+
+*********************************************************************************
+2. Crie uma função que calcula a velocidade média de um veiculo. Para isso leia a distancia e o tempo da viagem. Utilize arrow function
+
+const calculoVelocidade = (distancia, tempo) => {
+    return distancia / tempo;
+}
+
+alert(calculoVelocidade(200, 2));
